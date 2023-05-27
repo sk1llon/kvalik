8ae18fa28adbe57387e1549932c0533414c170f7
sk1llon <andrey_forsov.gg@mail.ru>
2023-05-27 17:42:53 +0300

    Сделано. Последнее задание не понял как делать.

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def main(number):
+    smallest_divisor = 0
+    for a in range(number,1,-1):
+        if number % a == 0:
+            smallest_divisor = a
+    print('Наименьший делитель, отличный от единицы:',smallest_divisor)
+number = int(input('Введите число: '))
+main(number)
