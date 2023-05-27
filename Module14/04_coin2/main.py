0785600cd08060c3dd0413446b8036b8a996a28e
sk1llon <andrey_forsov.gg@mail.ru>
2023-05-27 17:42:53 +0300

    Сделано.

--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def main(x,y,r):
+    if abs(x) > r or abs(y) > r:
+        print('Монетки в области нет!')
+    else:
+        print('Монетка где-то рядом!')
+x = float(input('Введите x координату: '))
+y = float(input('Введите y координату: '))
+radius = int(input('Введите радиус: '))
+main(x,y,radius)
