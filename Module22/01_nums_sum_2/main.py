7839d46a7b0237c16357ab6ba10e4a5ea23c106a
sk1llon <andrey_forsov.gg@mail.ru>
2023-07-23 18:10:51 +0300

    new files

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+numbers_file = open('numbers.txt', 'r')
+sum_of_numbers = 0
+for i_element in numbers_file:
+    if i_element != ' ' and i_element != '\n':
+        sum_of_numbers += int(i_element)
+answer_file = open('answer.txt', 'w')
+answer_file.write(str(sum_of_numbers))
+numbers_file.close()
+answer_file.close()
--- Module22/04_tournament/main.py
+++ Module22/04_tournament/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+def people_dictionary(dct):
+    for i_line in first_tour:
+        if i_line != '\n':
+            new_line = i_line.split()
+            dictionary[new_line[0], new_line[1]] = new_line[2]
+
+
+def second_tour_write(sec_tour, dct):
+    for keys, values in dct.items():
+        if int(values) > points:
+            name_and_surname = keys[1][0] + '. ' + keys[0] + ' ' + values + '\n'
+            sec_tour.write(name_and_surname)
+    return sec_tour
+
+
+def amount_of_people_in_second_tour():
+    sec_tour = open('second_tour.txt', 'r')
+    count = 0
+    for i_line in sec_tour:
+        if i_line != '\n':
+            count += 1
+    return count
+
+
+first_tour = open('first_tour.txt', 'r')
+second_tour = open('second_tour.txt', 'a')
+points = int(first_tour.read(2))
+dictionary = dict()
+people_dictionary(dictionary)
+second_tour = second_tour_write(second_tour, dictionary)
+amount_of_first_tour_winners = amount_of_people_in_second_tour()
+print('Содержимое файла second_tour.txt:\n', amount_of_first_tour_winners)
+second_tour.read()
+first_tour.close()
+second_tour.close()
