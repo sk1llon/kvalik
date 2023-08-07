666874e40faada03b840b0000ec0a8fd1d0c63d9
sk1llon <andrey_forsov.gg@mail.ru>
2023-08-07 13:25:56 +0300

    Сделано.

--- Module24/06_tic-tac-toe/main.py
+++ Module24/06_tic-tac-toe/main.py
@@ -1,1 +1,123 @@
-# TODO здесь писать код
+class Cell:
+    def __init__(self, num):
+        self.num = num
+        self.symbol = ' '
+
+    def __str__(self):
+        return self.symbol
+
+
+class Board:
+    def __init__(self):
+        self.cells = []
+        for i in range(9):
+            self.cells.append(Cell(i + 1))
+
+    def display(self):
+        for i in range(3):
+            print('-------------')
+            out = '| '
+            for j in range(3):
+                out += str(self.cells[i * 3 + j]) + ' | '
+            print(out)
+        print('-------------')
+
+    def update(self, cell_num, symbol):
+        if self.cells[cell_num - 1].symbol == ' ':
+            self.cells[cell_num - 1].symbol = symbol
+            return True
+        else:
+            return False
+
+    def is_game_over(self):
+        for i in range(3):
+            if self.cells[i * 3].symbol == self.cells[i * 3 + 1].symbol == self.cells[i * 3 + 2].symbol and self.cells[
+                i * 3].symbol != ' ':
+                return True
+        for i in range(3):
+            if self.cells[i].symbol == self.cells[i + 3].symbol == self.cells[i + 6].symbol and self.cells[
+                i].symbol != ' ':
+                return True
+        if self.cells[0].symbol == self.cells[4].symbol == self.cells[8].symbol and self.cells[0].symbol != ' ':
+            return True
+        if self.cells[2].symbol == self.cells[4].symbol == self.cells[6].symbol and self.cells[2].symbol != ' ':
+            return True
+        for cell in self.cells:
+            if cell.symbol == ' ':
+                return False
+        return True
+
+
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+        self.score = 0
+
+    def get_move(self):
+        try:
+            cell_num = int(input(self.name + ', введите номер клетки: '))
+            if cell_num == 0:
+                raise ValueError
+            return cell_num
+        except ValueError:
+            print('Ошибка! Введите число')
+            return self.get_move()
+
+
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.board = Board()
+        self.current_player = player1
+
+    def play_turn(self):
+        print(self.current_player.name + " ходит:\n")
+        self.board.display()
+        cell_num = self.current_player.get_move()
+        while not self.board.update(cell_num, self.current_player.symbol):
+            print('Клетка уже занята')
+            cell_num = self.current_player.get_move()
+
+        if self.board.is_game_over():
+            print(self.current_player.name + ' победил!\n')
+            self.board.display()
+            self.current_player.score += 1
+            return True
+
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
+
+        return False
+
+    def play_game(self):
+        print('Игра началась!')
+        self.board = Board()
+        self.current_player = self.player1
+        while not self.board.is_game_over():
+            if self.play_turn():
+                break
+
+        print('Счёт:')
+        print(self.player1.name + ': ' + str(self.player1.score))
+        print(self.player2.name + ': ' + str(self.player2.score))
+
+
+while True:
+    name1 = input('Введите имя первого игрока (X): ')
+    name2 = input('Введите имя второго игрока (O): ')
+    player1 = Player(name1, 'X')
+    player2 = Player(name2, 'O')
+    game = Game(player1, player2)
+    game.play_game()
+
+    again = input('Играем ещё раз? ')
+    if again.lower() != 'да':
+        break
+
+print('Спасибо за игру!')
+
+
--- Module24/07_Matrix/main.py
+++ Module24/07_Matrix/main.py
@@ -1,18 +1,38 @@
-# TODO здесь писать код
+import copy
 
 
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def add(self, matrix_2):
+        pass
+    def subtract(self, matrix_2):
+
+        pass
+
+    def multiply(self, matrix_2):
+
+        pass
+
+    def transpose(self):
+
+        pass
+    def columns(self):
+        for i_elem in self.matrix:
+            return len(i_elem)
+    def print_result(self):
+        for i_list in self.matrix:
+            for i_elem in i_list:
+                print(i_elem, end=' ')
+            print()
 
 
-# Примеры работы с классом:
+m1 = Matrix([[1, 2, 3], [4, 5, 6]])
 
-# Создание экземпляров класса Matrix
-m1 = Matrix(2, 3)
-m1.data = [[1, 2, 3], [4, 5, 6]]
+m2 = Matrix([[7, 8, 9], [10, 11, 12]])
 
-m2 = Matrix(2, 3)
-m2.data = [[7, 8, 9], [10, 11, 12]]
-
-# Тестирование операций
 print("Матрица 1:")
 print(m1)
 
@@ -25,8 +45,7 @@ print(m1.add(m2))
 print("Вычитание матриц:")
 print(m1.subtract(m2))
 
-m3 = Matrix(3, 2)
-m3.data = [[1, 2], [3, 4], [5, 6]]
+m3 = Matrix([[1, 2], [3, 4], [5, 6]])
 
 print("Умножение матриц:")
 print(m1.multiply(m3))
