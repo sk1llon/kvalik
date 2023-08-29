4c4a6546e9115b94d25f76b4f4e8c89015ade1f6
sk1llon <andrey_forsov.gg@mail.ru>
2023-08-29 20:12:01 +0300

    Сделано.

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,49 @@
-# TODO здесь писать код
+import math
+
+
+class MyMath:
+    @classmethod
+    def circle_len(cls, radius: int) -> float:
+        """
+        Метод для нахождения длины круга
+        :param radius:
+        :return:
+        """
+        return round(2 * math.pi * radius, 3)
+
+    @classmethod
+    def circle_sq(cls, radius: int) -> float:
+        """
+        Метод для нахождения площади круга
+        :param radius:
+        :return:
+        """
+        return round(math.pi * radius ** 2, 3)
+
+    @classmethod
+    def square_vol(cls, side: int) -> int:
+        """
+        Метод для нахождения объёма куба
+        :param side:
+        :return:
+        """
+        return side ** 3
+
+    @classmethod
+    def sphere_sur(cls, radius: int) -> float:
+        """
+        Метод для нахождения площади поверхности сферы
+        :param radius:
+        :return:
+        """
+        return round(4 * math.pi * radius ** 2, 3)
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+res_3 = MyMath.square_vol(side=4)
+res_4 = MyMath.sphere_sur(radius=4)
+print(res_1)
+print(res_2)
+print(res_3)
+print(res_4)
--- Module28/04_request_caching/main.py
+++ Module28/04_request_caching/main.py
@@ -1,5 +1,43 @@
-# TODO здесь писать код
+class LRUCache:
+    cache_list = list()
 
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current_capacity = 0
+
+    @property
+    def cache(self):
+        return
+
+    @cache.setter
+    def cache(self, new_elem):
+        key, value = new_elem
+        dictionary = dict()
+        if self.current_capacity < self.capacity:
+            self.current_capacity += 1
+            dictionary[key] = value
+            self.cache_list.append(dictionary)
+            dictionary.clear()
+        else:
+            self.cache_list.pop(0)
+            self.current_capacity -= 1
+
+    def print_cache(self):
+        for i_index in range(len(self.cache_list)):
+            for i_keys, i_values in self.cache_list[i_index].items():
+                print(i_keys, ':', i_values)
+
+    def get(self, key):
+        for i_index in range(len(self.cache_list)):
+            for i_dict in self.cache_list[i_index]:
+                for i_keys in i_dict.keys():
+                    if i_keys == key:
+                        if i_index != len(self.cache_list):
+                            self.cache_list[i_index], self.cache_list[i_index + 1] = self.cache_list[i_index + 1],\
+                                                                                     self.cache_list[i_index]
+                        return i_dict[key]
+                    else:
+                        return None
 
 
 # Создаем экземпляр класса LRU Cache с capacity = 3
