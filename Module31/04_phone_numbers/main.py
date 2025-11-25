af78122cd1e50da0b67774c95c776f6873527325
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:22:53 +0300

    Delete Module31 directory

--- Module31/04_phone_numbers/main.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-numbers_list = ['9999999999', '999999-999', '99999x9999']
-for index in range(len(numbers_list)):
-    result = re.findall(r'[8-9][0-9]{9}', numbers_list[index])
-    print('{index}-й номер: {value}'.format(index=index + 1,
-                                            value=('всё в порядке' if result else 'не подходит')))
