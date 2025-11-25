039f7663f1470d81555179631d518c9e66ba3144
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:21:22 +0300

    Delete Module18 directory

--- Module18/03_files/main.py
+++
@@ -1,7 +0,0 @@
-file_name = input('Введите название файла: ')
-if file_name.startswith('@' or '№' or '$' or '%' or '^' or '&' or "\ " or '*' or '(' or ')'):
-    print('Название начинается на один из специальных символов!')
-elif not file_name.endswith('.txt' or '.docx'):
-    print('Неверное расширение файла!')
-else:
-    print('Файл назван верно.')
--- Module18/04_capital_letters/main.py
+++
@@ -1,2 +0,0 @@
-text = input('Введите строку: ')
-print(text.title())
