a71f788ec5192439735eece0bae51e68b2316138
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:21:34 +0300

    Delete Module20 directory

--- Module20/03_players/main.py
+++
@@ -1,9 +0,0 @@
-players = {
-    ("Ivan", "Volkin"): (10, 5, 13),
-    ("Bob", "Robbin"): (7, 5, 14),
-    ("Rob", "Bobbin"): (12, 8, 2)
-}
-new_players = []
-for keys, values in players.items():
-    name, surname = keys
-    print(name, surname, values)
--- Module20/07_my_zip/main.py
+++
@@ -1,5 +0,0 @@
-string = input('Введите строку: ')
-new_tuple = input('Введите кортеж: ').split(', ')
-new_string = zip(string, new_tuple)
-for i_str in new_string:
-    print(i_str)
