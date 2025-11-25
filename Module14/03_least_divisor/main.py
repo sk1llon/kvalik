72ed49fa4140b1aad1aba6da09ae0d42542de81b
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:14:29 +0300

    Delete Module14 directory

--- Module14/03_least_divisor/main.py
+++
@@ -1,8 +0,0 @@
-def main(number):
-    smallest_divisor = 0
-    for a in range(number,1,-1):
-        if number % a == 0:
-            smallest_divisor = a
-    print('Наименьший делитель, отличный от единицы:',smallest_divisor)
-number = int(input('Введите число: '))
-main(number)
