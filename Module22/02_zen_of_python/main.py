22c2e800ccd425d9662a942e543c864545c3751e
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:21:46 +0300

    Delete Module22 directory

--- Module22/02_zen_of_python/main.py
+++
@@ -1,8 +0,0 @@
-zen_file = open('zen.txt', 'r')
-zen_list = []
-for i_line in zen_file:
-    zen_list.append(i_line)
-new_zen_list = zen_list[::-1]
-for i_line in new_zen_list:
-    print(i_line, end='')
-zen_file.close()
