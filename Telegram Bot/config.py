6c63a2bb43b28e32091a325a7962c057376f4c4f
sk1llon <133895852+sk1llon@users.noreply.github.com>
2025-11-26 01:14:57 +0300

    Delete Telegram Bot directory

--- Telegram Bot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-if not find_dotenv():
-    exit('There is no .env files here')
-else:
-    load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
